8a1b4a84dd4c5780088cec3b86e6b650b7a3ee89
rodneyringler <rodneyringler@gmail.com>
2025-10-06 10:09:49 -0400

    just adding some helping routes

---
+++ app/query/exercisetypes/route.ts
@@ -0,0 +1,20 @@
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function listExercises() {
+	// TODO: Update this query based on your exercises table schema
+	const data = await sql`
+    Select * from exercisetypes;
+  `;
+
+	return data;
+}
+
+export async function GET() {
+  try {
+  	return Response.json(await listExercises());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
+}
--- app/query/invoices/route.ts
+++ app/query/invoices/route.ts
@@ -7,17 +7,13 @@ async function listInvoices() {
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    LIMIT 2;
   `;
 
 	return data;
 }
 
 export async function GET() {
-  // return Response.json({
-  //   message:
-  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  // });
   try {
   	return Response.json(await listInvoices());
   } catch (error) {
