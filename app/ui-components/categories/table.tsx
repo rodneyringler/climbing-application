1b3d873ad01b52809a1566e27ff4cb9a389a2a7d
Rodney Ringler <rodneyringler@Rodneys-Air.attlocal.net>
2025-12-26 20:28:56 -0500

    test

--- app/ui-components/categories/table.tsx
+++ app/ui-components/categories/table.tsx
@@ -12,16 +12,18 @@ export default function CategoriesTable({
 }) {
     const colorValue = 200;
     return (
-        <div >
+        <div className="grid grid-rows-[auto_1fr_auto] h-screen">
             <header className="bg-stone-100 p-4 rounded-md">Get Started With Pre-Made Plans:</header>
-            <main >
+            <main className="bg-white p-4 rounded-lg overflow-auto">
                 {categories?.map((category) => (
-                    <div key={category.id} >
+                    <div key={category.id} className="md:h-220 m-1 hover:ml-8 hover:w-full rounded-md bg-stone-400">
                         <div>
                             <h3 className={`${lusitana.className} text-lg left-2`}>{category.name}</h3>
                             <p>{category.imageUrl}</p>
-                            <Image src={`/endurance.jpg`} alt={category.description} width={100} height={100} />
+                            <img src={`/${category.imageUrl}`} alt={category.description} width={100} height={100}/>
                         </div>
+                        
+
                     </div>
                 ))}
             </main>
